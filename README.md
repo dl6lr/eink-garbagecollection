c5926de86e7fc257ae90019684e4ebb193c74e2d
Bernd Laengerich <bernd@t630.fritz.box>
2024-02-16 10:52:36 +0100

    initial release

---
+++ README.md
@@ -0,0 +1,44 @@
+# eink-garbagecalendar
+
+Create an image suitable to be displayed on a SoluM ST-GR29000 ePaper price tag.
+The image shows if today or tomorrow a garbage can has to be placed for collection. 
+
+# Prerequisites
+
+The python script currently uses Python 3.9.2 with PyYAML 6.0, Pillow 9.3.0, pyowm 3.3.0 and matplotlib 3.6.2
+Especially if you run the scripts from cron, make sure the packages are installed in the system, not in the user path only.
+Make sure you have the right version installed in the system paths, it is quite confusing if you test the scripts as a regular user and exhibit strange behaviour when run from cron due to different versions installed in the system and user path. Don't ask how I know...
+To access OpenWeatherMap, you need to create a free account and generate an API key.
+
+    sudo pip install pyowm Pillow matplotlib pyyaml
+
+# The generated image
+
+A generated image looks similar to the following example:
+
+![collection display](https://github.com/dl6lr/eink-garbagecollection/blob/main/abfuhr.jpg "collection display")
+
+# Configuration
+
+Configuration is done in config.yml that resides beneath owm.py. If you download the package for the first time, copy the sample file to config.yml and edit it.
+Paste your OWM API key to config.yml and set your city and country:
+
+
+    output:
+        filename: collection.jpg
+
+    fonts:
+        small: fonts/VeraSe12.pil
+        big: fonts/VeraSe18.pil
+
+    openepaperlink:
+        apip: 192.168.178.30
+        mac: 026d29c13b16
+
+
+Running the script with:
+
+    python garbage.py
+
+should give no error message and the file garbage.jpg should be updated and uploaded to the access point.  The image fits the price tags orientation with the barcode to the left.
+
---
+++ garbage.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 12 12:18:43 2022
+
+@author: omega
+"""
+# imports
+from icalendar import Calendar, Event, vCalAddress, vText
+from datetime import datetime, timedelta, time
+from PIL import Image, ImageDraw, ImageFont
+from pathlib import Path
+from collections import namedtuple
+import os
+import pytz
+import time
+import re
+import requests
+import yaml
+ 
+# init the calendar
+cal = Calendar()
+
+Display_Size_x=296
+Display_Size_y=128
+config = yaml.safe_load(open("config.yml"))
+destination_file = config['output']['filename']
+
+def generate_abhol_image(summary, datum):
+    muster=Image.new("RGB",(296,128),color=(255,255,255))
+    named_tuple = time.localtime() # get struct_time
+    time_string = time.strftime("%d.%m.%Y, %H:%M:%S", named_tuple)
+    im = ImageDraw.Draw(muster)
+    font = ImageFont.load('fonts/VeraMono30.pil')
+    font_klein = ImageFont.load('fonts/VeraMono8.pil')
+    
+
+    _, _, w, h = im.textbbox((0, 0), summary, font=font)
+    im.text((((Display_Size_x/2)-w/2),Display_Size_y/2-h/2-30), summary ,fill=(0, 0, 0),font=font)
+    
+    _, _, w, h = im.textbbox((0, 0), datum, font=font_klein)
+    im.text((((Display_Size_x/2)-w/2),80), datum ,fill=(0, 0, 0),font=font_klein)
+    
+    _, _, w, h = im.textbbox((0, 0), time_string, font=font_klein)
+    im.text((((Display_Size_x/2)-w/2),100), time_string ,fill=(0, 0, 0),font=font_klein)
+    
+    # muster=muster.rotate(angle=90,expand="true")
+    if (destination_file.upper().endswith('.JPG')):
+        muster.save(destination_file, 'JPEG', quality="maximum")
+    else:
+        muster.save(destination_file)
+
+
+def generate_empty_image():
+    muster=Image.new("RGB",(296,128),color=(255,255,255))
+    named_tuple = time.localtime() # get struct_time
+    time_string = time.strftime("%d.%m.%Y, %H:%M:%S", named_tuple)
+    im = ImageDraw.Draw(muster)
+    font_klein = ImageFont.load('fonts/VeraMono8.pil')
+    
+    _, _, w, h = im.textbbox((0, 0), time_string, font=font_klein)
+    im.text((((Display_Size_x/2)-w/2),100), time_string ,fill=(0, 0, 0),font=font_klein)
+    
+    # muster=muster.rotate(angle=90,expand="true")
+    if (destination_file.upper().endswith('.JPG')):
+        muster.save(destination_file, 'JPEG', quality="maximum")
+    else:
+        muster.save(destination_file)
+
+#
+# push image to OpenEPaperLink AP
+#
+def pushImage(image_path, cfg_oepl):
+  # Prepare the HTTP POST request
+  url = "http://" + cfg_oepl['apip'] + "/imgupload"
+  payload = {"dither": 0, "mac": cfg_oepl['mac']}  # Additional POST parameter
+  files = {"file": open(image_path, "rb")}  # File to be uploaded
+
+  # Send the HTTP POST request
+  response = requests.post(url, data=payload, files=files)
+
+  # Check the response status
+  if response.status_code == 200:
+    print("Image uploaded successfully!")
+  else:
+    print("Failed to upload the image.")
+
+
+
+generate_empty_image()
+today = datetime.today().date()
+e = open('abfuhrtermine.ics', 'rb')
+ecal = cal.from_ical(e.read())
+for component in ecal.walk():
+    #print(component.name)
+    if component.name == "VEVENT":
+        summary = component.get('summary')
+        shortsummary = re.search(r'^[a-zA-Z]+|$', summary).group()
+        #print ('VEVENT: ', shortsummary)
+        #print ('VEVENT: ', component.get('dtstart').dt)
+        abfuhrtag = component.get('dtstart').dt
+        nachrichttag = abfuhrtag - timedelta(days=1)
+        nachrichttag = abfuhrtag - timedelta(days=1)
+        #print(today, nachrichttag)
+        if(today==nachrichttag or today==abfuhrtag):
+            summary = component.get('summary')
+            shortsummary = re.search(r'^[a-zA-Z]+|$', summary).group()
+            #print ('TODAY: ', component.get('summary'))
+            #print ('TODAY: ', component.get('dtstart').dt)
+            datetext = "HEUTE" if today==abfuhrtag else component.get('dtstart').dt
+            generate_abhol_image(str(shortsummary), str(datetext))
+        
+        
+e.close()
+if ('openepaperlink' in config):
+    pushImage(destination_file, config['openepaperlink'])
